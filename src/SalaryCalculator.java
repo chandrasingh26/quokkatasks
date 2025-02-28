5fc17d053069449836dba939a25898157789ecb0
chandrasingh18 <122686704+chandrasingh18@users.noreply.github.com>
2025-02-28 17:21:42 +0530

    base salary displayed to output

--- src/SalaryCalculator.java
+++ src/SalaryCalculator.java
@@ -6,13 +6,16 @@ public class SalaryCalculator {
 
         System.out.print("Enter base salary: ");
         double baseSalary = scanner.nextDouble();
+        System.out.println("base salary: " + baseSalary);
 
         System.out.print("Enter allowance: ");
         double allowance = scanner.nextDouble();
+        System.out.println("allowance: " + allowance);
 
         double grossSalary = baseSalary + allowance;
 
         double tax = 0.1 * grossSalary;
+        System.out.println("tax: " + tax);
         double netSalary = grossSalary - tax;
 
         double finalSalary = (grossSalary > 50000) ? netSalary : grossSalary ;
